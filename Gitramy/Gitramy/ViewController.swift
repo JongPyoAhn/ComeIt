971cffd8cdb654b4b9487ec1e7043be90bf0edb7
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-10-04 20:48:00 +0900

    first Commit

---
+++ Gitramy/Gitramy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gitramy
+//
+//  Created by 안종표 on 2021/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
