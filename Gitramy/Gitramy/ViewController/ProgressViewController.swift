595ffa869026f327a25588c10a05de5221d392ec
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-10-04 22:58:57 +0900

    prototype

---
+++ Gitramy/Gitramy/ViewController/ProgressViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressViewController.swift
+//  Gitramy
+//
+//  Created by 안종표 on 2021/10/04.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
