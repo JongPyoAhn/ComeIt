39efa7cd33f806f04301ecc43adc63bc2a7cc88e
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-10-11 13:29:21 +0900

    add Charts

--- Gitramy/Gitramy/ViewController/ChartViewController.swift
+++ Gitramy/Gitramy/ViewController/ChartViewController.swift
@@ -9,36 +9,89 @@ import UIKit
 import Kingfisher
 import SwiftSVG
 import Charts
-class ChartViewController: UIViewController {
+
+class ChartViewController: UIViewController, ChartViewDelegate {
 
 
     @IBOutlet weak var contributionImageView: UIImageView!
     
-    @IBOutlet weak var lineChartView: LineChartView!
-    var numbers: [Double] = []
+    @IBOutlet weak var repositoryChartView: LineChartView!
     
+    //x축을 레파지토리 이름 받아오기
+    let repositoryNames = ["Jan", "Feb", "Mar", "Apr", "May"]
+    
+    //모든 레포지토리 데이터 받아와서 y축을 총커밋수
+    let repositoryValues: [ChartDataEntry] = [
+        ChartDataEntry(x: 0, y: 20.0),
+        ChartDataEntry(x: 1, y: 30.0),
+        ChartDataEntry(x: 2, y: 40.0),
+        ChartDataEntry(x: 3, y: 45.0),
+        ChartDataEntry(x: 4, y: 50.0)
+    ]
     
     let imageURL = URL(string: "https://ghchart.rshah.org/JongpyoAhn")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        numbers = [40, 30, 20, 15, 10]
+        setLineChartView()
+        setData(repositoryValues)
        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        self.contributionImageView.kf.setImage(with: imageURL)
         let fistBump = UIView(SVGURL: imageURL!)
         self.contributionImageView.addSubview(fistBump)
     }
-    
-    func lineGraph() {
-        
-    }
-    
-    
-
 }
 
 
+//차트관련
+extension ChartViewController {
+    
+    func setData(_ values: [ChartDataEntry]){
+        let set1 = LineChartDataSet(entries: values, label: "Repository")
+//        set1.mode = .stepped //선의 종류
+        set1.drawCirclesEnabled = false //선 꼭짓점에 생기던 동그란원이 사라짐
+        set1.lineWidth = 3 //선의 굵기
+        set1.setColor(.white) //선의 색깔
+        set1.drawHorizontalHighlightIndicatorEnabled = false //누르고 쭉 당기면 노란줄생기는거 없어짐.
+        set1.highlightColor = .systemRed //누르면서 움직이면 빨간색나오게함
+        
+        let data = LineChartData(dataSet: set1)
+        data.setDrawValues(false)
+        repositoryChartView.data = data
+    }
+    
+    //꺽은선그래프 꾸미기
+    func setLineChartView(){
+        repositoryChartView.backgroundColor = .darkGray
+        repositoryChartView.rightAxis.enabled = false
+        
+        let yAxis = repositoryChartView.leftAxis
+        let xAxis = repositoryChartView.xAxis
+        
+        yAxis.gridColor = .clear //격자 선 지우기
+        xAxis.gridColor = .clear
+
+        yAxis.labelFont = .boldSystemFont(ofSize: 12) //왼쪽 y축 눈금의 폰트를 설정
+        yAxis.setLabelCount(repositoryValues.count, force: false) //왼쪽 y축 눈금의 수를 설정
+        yAxis.labelTextColor = .white //왼쪽 y축 눈금의 색을 설정
+        yAxis.axisLineColor = .white //왼쪽 y축 눈금선의 색을 설정
+        xAxis.labelPosition = .bottom //위에 있던 x축 눈금이 아래로 내려옴.
+        xAxis.labelFont = .boldSystemFont(ofSize: 12) //x축 폰트 설정
+        xAxis.setLabelCount(repositoryValues.count, force: true) //x축 레이블의 수를 설정
+        xAxis.labelTextColor = .white //x축 글자색깔
+        xAxis.axisLineColor = .systemBlue //x축 눈금선의 색을 설정
+        
+        
+        xAxis.valueFormatter = IndexAxisValueFormatter(values:repositoryNames)
+        xAxis.granularity = 1
+        
+        
+        repositoryChartView.animate(xAxisDuration: 2.5) //애니메이션 설정
+    }
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print(entry)
+    }
+}
