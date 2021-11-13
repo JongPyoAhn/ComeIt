04eaa88736a7c17930e5820bb705ce787f8d8cf4
JongPyoAhn <68585628+JongPyoAhn@users.noreply.github.com>
2021-11-13 15:18:12 +0900

    add

---
+++ Gitramy/Gitramy/ViewController/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  profileViewController.swift
+//  Gitramy
+//
+//  Created by 안종표 on 2021/11/12.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
